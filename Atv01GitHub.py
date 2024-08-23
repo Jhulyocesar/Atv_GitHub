32447b5b800d48f0f75503b87085d0dd2b427fe4
Júlio César <jhulyocessarr@gmail.com>
2024-08-23 17:29:22 -0300

    Perguntas

---
+++ Atv01GitHub.py
@@ -0,0 +1,5 @@
+pergunta01 = input('Telefonou para a vítima? ')
+pergunta02 = input('Esteve no local? ')
+pergunta03 = input('Mora perto da vítima? ')
+pergunta04 = input('Devia para a vítima? ')
+pergunta05 = input('Já trabalhou com a vítima? ')
